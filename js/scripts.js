3a69ccdecb35eee79da31ee98d732b147a63ff5b
brantchyoga <brantrporter@gmail.com>
2018-02-06 14:32:24 -0800

    added form input of names and have quiz inputing radio buttons in correct spots

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,4 @@
-$("#accordion1, #accordion2, #accordion3").accordion({
-  active: true,
-  collapsible: true});
-$("#tabs").tabs({
-  active: false,
-  collapsible: true
-});
+
 var genKnowEasy = [];
 var genKnowMed =[];
 var genKnowHard =[];
@@ -15,78 +9,54 @@ var mythEasy =[];
 var mythMed =[];
 var mythHard=[];
 
-function answersInField(obj) {
-  var cA = obj.correct_answer;
-  var iA = obj.incorrect_answers;
-  iA.splice()
-  let randomNumber = Math.floor(Math.random() * 4);
-  iA.splice(randomNumber, 0, cA);
-  console.log(cA);
-  console.log(iA);
-  var radioBtn = $('<input type="radio" name="question0" class="input"><label for="question0">Toggle this custom radio</label>');
-  for()
+var players = {
+  player1: {
+		player:'',
+		score: "score"
+	},
+	player2: {
+		player:'',
+		score: "score"
+	}
 }
+var turn = 0;
+
+function names() {
+  var nameOne = $('#name1').val();
+  var nameTwo = $('#name2').val();
+  $('#name2').val('');
+  $('#name1').val('');
+  let randomNumber = Math.floor(Math.random()*2);
+  if(randomNumber === 1) {
+    players.player1.player = nameOne;
+    players.player2.player = nameTwo;
+  } else {
+    players.player1.player = nameTwo;
+    players.player2.player = nameOne;
+  }
+  console.log(players.player1.player, players.player2.player);
+}
+
 function reset(obj) {
   //Going to need to change the random number to another random number
-    randomQuestion = Math.floor((Math.random() * obj.length)-1);
 }
 
+function score(category, value){
+  //button click (#category+value) find correct answer and add that score to players turn
+  //if incorrect_answers minus
+}
 
 $(document).ready(function(){
-  $.get('js/genknow_easy.json').done(function(data) {
-    genKnowEasy = data.results;
-    console.log(genKnowEasy);
-    let randomQuestion = Math.floor((Math.random() * genKnowEasy.length)-1);
-    $('#accordion1 .question1').html(genKnowEasy[randomQuestion].question);
-    answersInField(genKnowEasy[randomQuestion]);
-  });
+  $('form[name=names] button').on('click', names)
 
-  $.get('js/genknow_med.json').done(function(data) {
-    genKnowMed = data.results;
-    let randomQuestion = Math.floor((Math.random() * genKnowMed.length)-1);
-    $('#accordion1 .question2').html(genKnowMed[randomQuestion].question);
-  });
-
-  $.get('js/genknow_hard.json').done(function(data) {
-    genKnowHard = data.results;
-    let randomQuestion = Math.floor((Math.random() * genKnowHard.length)-1);
-    $('#accordion1 .question3').html(genKnowHard[randomQuestion].question)
-  });
-
-  $.get('js/genknow_easy.json').done(function(data) {
-    entFilmEasy = data.results;
-    let randomQuestion = Math.floor((Math.random() * entFilmEasy.length)-1);
-    $('#accordion2 .question1').html(entFilmEasy[randomQuestion].question)
-  });
-
-  $.get('js/genknow_med.json').done(function(data) {
-    entFilmMed = data.results;
-    let randomQuestion = Math.floor((Math.random() * entFilmMed.length)-1);
-    $('#accordion2 .question2').html(entFilmMed[randomQuestion].question)
-  });
-
-  $.get('js/genknow_hard.json').done(function(data) {
-    entFilmHard = data.results;
-    let randomQuestion = Math.floor((Math.random() * entFilmHard.length)-1);
-    $('#accordion2 .question3').html(entFilmHard[randomQuestion].question)
-  });
-
-  $.get('js/genknow_easy.json').done(function(data) {
-    mythEasy = data.results;
-    let randomQuestion = Math.floor((Math.random() * mythEasy.length)-1);
-    $('#accordion3 .question1').html(mythEasy[randomQuestion].question)
-  });
-
-  $.get('js/genknow_med.json').done(function(data) {
-    mythMed = data.results;
-    let randomQuestion = Math.floor((Math.random() * mythMed.length)-1);
-    $('#accordion3 .question2').html(mythMed[randomQuestion].question)
-  });
-
-  $.get('js/genknow_hard.json').done(function(data) {
-    mythHard = data.results;
-    let randomQuestion = Math.floor((Math.random() * mythHard.length)-1);
-    $('#accordion3 .question3').html(mythHard[randomQuestion].question)
-  });
+  $('button #1100').on('click', function(){});
+  $('button #1200').on('click', function(){});
+  $('button #1300').on('click', function(){});
+  $('button #2100').on('click', function(){});
+  $('button #2200').on('click', function(){});
+  $('button #2300').on('click', function(){});
+  $('button #3100').on('click', function(){});
+  $('button #3200').on('click', function(){});
+  $('button #3300').on('click', function(){});
 
 });
