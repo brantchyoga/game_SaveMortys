aef21957781b977556b08b2d7fa56f38929a3e1f
brantchyoga <brantrporter@gmail.com>
2018-02-05 17:08:59 -0800

    Working on function that puts answers into html as radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -15,70 +15,73 @@ var mythEasy =[];
 var mythMed =[];
 var mythHard=[];
 
-// function shuffleObject(array) {
-//   var temp;
-// 		for(var i = array.length-1;i>=0;i--) {
-// 			var randomNUm = Math.floor(Math.random() * (i + 1));
-// 			temp = array[i];
-// 			array[i] = array[randomNUm];
-// 			array[randomNUm] = temp;
-// 		}
-// }
+function answersInField(obj) {
+  var cA = obj.correct_answer;
+  var iA = obj.incorrect_answers;
+  console.log(cA);
+  console.log(iA);
+}
+function reset(obj) {
+  //Going to need to change the random number to another random number
+    randomQuestion = Math.floor((Math.random() * obj.length)-1);
+}
 
 
 $(document).ready(function(){
   $.get('js/genknow_easy.json').done(function(data) {
     genKnowEasy = data.results;
-    let randomQuest = Math.floor((Math.random() * genKnowEasy.length)-1);
-    $('#accordion1 .question1').html(genKnowEasy[randomQuest].question);
+    console.log(genKnowEasy);
+    let randomQuestion = Math.floor((Math.random() * genKnowEasy.length)-1);
+    $('#accordion1 .question1').html(genKnowEasy[randomQuestion].question);
+    answersInField(genKnowEasy[randomQuestion]);
   });
 
   $.get('js/genknow_med.json').done(function(data) {
     genKnowMed = data.results;
-    let randomQuest = Math.floor((Math.random() * genKnowMed.length)-1);
-    $('#accordion1 .question2').html(genKnowMed[randomQuest].question);
+    let randomQuestion = Math.floor((Math.random() * genKnowMed.length)-1);
+    $('#accordion1 .question2').html(genKnowMed[randomQuestion].question);
   });
 
   $.get('js/genknow_hard.json').done(function(data) {
     genKnowHard = data.results;
-    let randomQuest = Math.floor((Math.random() * genKnowHard.length)-1);
-    $('#accordion1 .question3').html(genKnowHard[randomQuest].question)
+    let randomQuestion = Math.floor((Math.random() * genKnowHard.length)-1);
+    $('#accordion1 .question3').html(genKnowHard[randomQuestion].question)
   });
 
   $.get('js/genknow_easy.json').done(function(data) {
     entFilmEasy = data.results;
-    let randomQuest = Math.floor((Math.random() * entFilmEasy.length)-1);
-    $('#accordion2 .question1').html(entFilmEasy[randomQuest].question)
+    let randomQuestion = Math.floor((Math.random() * entFilmEasy.length)-1);
+    $('#accordion2 .question1').html(entFilmEasy[randomQuestion].question)
   });
 
   $.get('js/genknow_med.json').done(function(data) {
     entFilmMed = data.results;
-    let randomQuest = Math.floor((Math.random() * entFilmMed.length)-1);
-    $('#accordion2 .question2').html(entFilmMed[randomQuest].question)
+    let randomQuestion = Math.floor((Math.random() * entFilmMed.length)-1);
+    $('#accordion2 .question2').html(entFilmMed[randomQuestion].question)
   });
 
   $.get('js/genknow_hard.json').done(function(data) {
     entFilmHard = data.results;
-    let randomQuest = Math.floor((Math.random() * entFilmHard.length)-1);
-    $('#accordion2 .question3').html(entFilmHard[randomQuest].question)
+    let randomQuestion = Math.floor((Math.random() * entFilmHard.length)-1);
+    $('#accordion2 .question3').html(entFilmHard[randomQuestion].question)
   });
 
   $.get('js/genknow_easy.json').done(function(data) {
     mythEasy = data.results;
-    let randomQuest = Math.floor((Math.random() * mythEasy.length)-1);
-    $('#accordion3 .question1').html(mythEasy[randomQuest].question)
+    let randomQuestion = Math.floor((Math.random() * mythEasy.length)-1);
+    $('#accordion3 .question1').html(mythEasy[randomQuestion].question)
   });
 
   $.get('js/genknow_med.json').done(function(data) {
     mythMed = data.results;
-    let randomQuest = Math.floor((Math.random() * mythMed.length)-1);
-    $('#accordion3 .question2').html(mythMed[randomQuest].question)
+    let randomQuestion = Math.floor((Math.random() * mythMed.length)-1);
+    $('#accordion3 .question2').html(mythMed[randomQuestion].question)
   });
 
   $.get('js/genknow_hard.json').done(function(data) {
     mythHard = data.results;
-    let randomQuest = Math.floor((Math.random() * mythHard.length)-1);
-    $('#accordion3 .question3').html(mythHard[randomQuest].question)
+    let randomQuestion = Math.floor((Math.random() * mythHard.length)-1);
+    $('#accordion3 .question3').html(mythHard[randomQuestion].question)
   });
 
 });
