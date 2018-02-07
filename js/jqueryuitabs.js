6c101ca19437c7e01e3847f5c8d420e5338d223b
brantchyoga <brantrporter@gmail.com>
2018-02-06 19:35:04 -0800

    Changed html and javascript functions. Answers and questions appear on page by combining ajax calls into a object

--- js/jqueryuitabs.js
+++ js/jqueryuitabs.js
@@ -6,7 +6,7 @@ function answersInField(obj, category, value) {
   let randomNumber = Math.floor(Math.random() * 4);
   iA.splice(randomNumber, 0, cA);
 
-  $('#accordion'+category+' .'+value+'').append('<form name="'+category+value+'"></form>');
+  $('#'+category+' .'+value+'').append('<form name="'+category+value+'"></form>');
   $('form[name='+category+value+']').append('<button id="'+category+value+'">Final Answer!</button>');
 
   for(var i = 0;i < iA.length;i++) {
@@ -17,15 +17,7 @@ function answersInField(obj, category, value) {
 }
 
 $(document).ready(function(){
-  $("#accordion1").accordion({
-    active: true,
-    collapsible: true});
-  $("#accordion2").accordion({
-      active: true,
-      collapsible: true});
-  $("#accordion3").accordion({
-        active: true,
-        collapsible: true});
+
   $("#tabs").tabs({
     active: false,
     collapsible: true
@@ -33,83 +25,38 @@ $(document).ready(function(){
 
   $.get('js/genknow_easy.json').done(function(data) {
     genKnowEasy = data.results;
-    let value = 100;
-    let category = 1;
-    randomQuestion = Math.floor(Math.random() * genKnowEasy.length);
-    $('#accordion1 .'+value+'').html(genKnowEasy[randomQuestion].question);
-    answersInField(genKnowEasy[randomQuestion], category, value);
   });
 
   $.get('js/genknow_med.json').done(function(data) {
     genKnowMed = data.results;
-    let value = 200;
-    let category = 1;
-    randomQuestion = Math.floor(Math.random() * genKnowMed.length);
-    $('#accordion1 .'+value+'').html(genKnowMed[randomQuestion].question);
-    answersInField(genKnowMed[randomQuestion], category, value);
   });
 
   $.get('js/genknow_hard.json').done(function(data) {
     genKnowHard = data.results;
-    let value = 300;
-    let category = 1;
-    randomQuestion = Math.floor(Math.random() * genKnowHard.length);
-    $('#accordion1 .'+value+'').html(genKnowHard[randomQuestion].question);
-    answersInField(genKnowHard[randomQuestion], category, value);
   });
 
   $.get('js/entfilm_easy.json').done(function(data) {
     entFilmEasy = data.results;
-    let value = 100;
-    let category = 2;
-    randomQuestion = Math.floor(Math.random() * entFilmEasy.length);
-    $('#accordion2 .'+value+'').html(entFilmEasy[randomQuestion].question);
-    answersInField(entFilmEasy[randomQuestion], category, value);
   });
 
   $.get('js/entfilm_med.json').done(function(data) {
     entFilmMed = data.results;
-    let value = 200;
-    let category = 2;
-    randomQuestion = Math.floor(Math.random() * entFilmMed.length);
-    $('#accordion2 .'+value+'').html(entFilmMed[randomQuestion].question);
-    answersInField(entFilmMed[randomQuestion], category, value);
   });
 
   $.get('js/entfilm_hard.json').done(function(data) {
     entFilmHard = data.results;
-    let value = 300;
-    let category = 2;
-    randomQuestion = Math.floor(Math.random() * entFilmHard.length);
-    $('#accordion2 .'+value+'').html(entFilmHard[randomQuestion].question);
-    answersInField(entFilmHard[randomQuestion], category, value);
   });
 
   $.get('js/myth_easy.json').done(function(data) {
     mythEasy = data.results;
-    let value = 100;
-    let category = 3;
-    randomQuestion = Math.floor(Math.random() * mythEasy.length);
-    $('#accordion3 .'+value+'').html(mythEasy[randomQuestion].question);
-    answersInField(mythEasy[randomQuestion], category, value);
   });
 
   $.get('js/myth_med.json').done(function(data) {
     mythMed = data.results;
-    let value = 200;
-    let category = 3;
-    randomQuestion = Math.floor(Math.random() * mythMed.length);
-    $('#accordion3 .'+value+'').html(mythMed[randomQuestion].question);
-    answersInField(mythMed[randomQuestion], category, value);
   });
 
   $.get('js/myth_hard.json').done(function(data) {
     mythHard = data.results;
-    let value = 300;
-    let category = 3;
-    randomQuestion = Math.floor(Math.random() * mythHard.length);
-    $('#accordion3 .'+value+'').html(mythHard[randomQuestion].question);
-    answersInField(mythHard[randomQuestion], category, value);
   });
 
 });
--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,16 @@ var mythEasy =[];
 var mythMed =[];
 var mythHard=[];
 
+var correctAnswer;
+
 var players = {
   player1: {
 		player:'',
-		score: "score"
+		score: 0
 	},
 	player2: {
 		player:'',
-		score: "score"
+		score: 0
 	}
 }
 var turn = 0;
@@ -35,28 +37,85 @@ function names() {
     players.player2.player = nameOne;
   }
   console.log(players.player1.player, players.player2.player);
+  $('#names').hide();
+  $('main').show();
+  $('#scoreboard').show();
+  $('.player1').text(players.player1.player);
+  $('.player2').text(players.player2.player);
 }
 
 function reset(obj) {
   //Going to need to change the random number to another random number
 }
 
-function score(category, value){
-  //button click (#category+value) find correct answer and add that score to players turn
-  //if incorrect_answers minus
+function addScore(){
+  console.log('working');
+  
+}
+function showQuestion() {
+  var map = {
+    category1: {
+      100: genKnowEasy,
+      200: genKnowMed,
+      300: genKnowHard
+    },
+    category2: {
+      100: entFilmEasy,
+      200: entFilmMed,
+      300: entFilmHard
+    },
+    category3: {
+      100: mythEasy,
+      200: mythMed,
+      300: mythHard
+    }
+  };
+  $('.col').off();
+  var level = parseInt(this.classList[1]);
+  var category = $(this).parent().attr('id');
+  var objects = map[category][level];
+
+  let randomQuestion = Math.floor(Math.random() * objects.length);
+  let randomNumber = Math.floor(Math.random() * 2);
+
+  var object = objects[randomQuestion];
+
+  correctAnswer = object.correct_answer;
+  var incorrectAnswers = object.incorrect_answers;
+  incorrectAnswers.splice(randomNumber, 0, correctAnswer);
+  var div = $('<div class="questions">');
+  div.html(object.question);
+  console.log(div);
+  $(this).append(div);
+  $('.questions').append($('<form class="answers"><button type="button" class="submit">Final Answer!</button></form>'));
+  incorrectAnswers.forEach(function(answer){
+    $('.answers').prepend($('<div class="radio"><input type="radio" value="'+answer+'"><label>'+answer+'</label></div>'));
+  });
+  $('.submit').on('click', addScore);
 }
 
 $(document).ready(function(){
-  $('form[name=names] button').on('click', names)
+  $('#scoreboard').hide();
+  $('main').hide();
+  $('form[name=names] button').on('click', names);
+  $('#category1 .100').on('click', showQuestion);
+  $('#category1 .200').on('click', showQuestion);
+  $('#category1 .300').on('click', showQuestion);
+  $('#category2 .100').on('click', showQuestion);
+  $('#category2 .200').on('click', showQuestion);
+  $('#category2 .300').on('click', showQuestion);
+  $('#category3 .100').on('click', showQuestion);
+  $('#category3 .200').on('click', showQuestion);
+  $('#category3 .300').on('click', showQuestion);
 
-  $('button #1100').on('click', function(){});
-  $('button #1200').on('click', function(){});
-  $('button #1300').on('click', function(){});
-  $('button #2100').on('click', function(){});
-  $('button #2200').on('click', function(){});
-  $('button #2300').on('click', function(){});
-  $('button #3100').on('click', function(){});
-  $('button #3200').on('click', function(){});
-  $('button #3300').on('click', function(){});
+  // $('button #1100').on('click', function(){});
+  // $('button #1200').on('click', function(){});
+  // $('button #1300').on('click', function(){});
+  // $('button #2100').on('click', function(){});
+  // $('button #2200').on('click', function(){});
+  // $('button #2300').on('click', function(){});
+  // $('button #3100').on('click', function(){});
+  // $('button #3200').on('click', function(){});
+  // $('button #3300').on('click', function(){});
 
 });
